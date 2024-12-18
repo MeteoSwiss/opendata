895edd22f329b96bffde3296010f7e2ac0f2be4a
Stefan Oderbolz <oderbolz@gmail.com>
2024-12-18 09:16:24 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -35,7 +35,7 @@ const config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en', 'de'],
+    locales: ['en', 'de', 'fr', 'it'],
   },
 
   presets: [
