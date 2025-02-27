211152584e629d110c05b3566d3005d49f6c30d2
Stefan Oderbolz <oderbolz@gmail.com>
2025-02-27 16:41:42 +0000

    Do not fail on broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -27,7 +27,7 @@ const config = {
 
   trailingSlash: false,
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
