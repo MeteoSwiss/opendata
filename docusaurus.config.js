98e4d72c8a3559ccb42e809a6718783fcf988649
Stefan Oderbolz <oderbolz@gmail.com>
2025-03-25 13:21:15 +0100

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,10 +15,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://meteoswiss.github.io/',
+  url: 'https://http://opendatadocs.meteoswiss.ch/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/opendata/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
