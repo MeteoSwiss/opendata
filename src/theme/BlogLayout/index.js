5fbb5b178d1a64e6769426f6df8d88c63d6a6a96
Stefan Oderbolz <oderbolz@gmail.com>
2025-05-20 14:50:12 +0000

    Update Changelog translations

--- src/theme/BlogLayout/index.js
+++ src/theme/BlogLayout/index.js
@@ -2,11 +2,10 @@ import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import BlogSidebar from '@theme/BlogSidebar';
-import useBaseUrl from '@docusaurus/useBaseUrl';
 export default function BlogLayout(props) {
   const {sidebar, toc, children, ...layoutProps} = props;
   const hasSidebar = sidebar && sidebar.items.length > 0;
-  const rss_url = useBaseUrl('/changelog/rss.xml');
+  const rss_url = "https://opendatadocs.meteoswiss.ch/changelog/rss.xml"; // don't se useBaseUrl() here since this URL must not be localized
   return (
     <Layout {...layoutProps}>
       <div className="container margin-vert--lg">
